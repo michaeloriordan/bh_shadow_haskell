b631a5f263b7cbc1fe3c443bfd4a2e1baa02fb5d
michaeloriordan <michaeloriordan4@gmail.com>
2018-04-25 11:02:47 +0100

    Small

--- Main.hs
+++ Main.hs
@@ -4,7 +4,7 @@ import BH_Shadow (init_camera, init_photons, propagate_photons, data_to_save)
 camera          = init_camera
 initial_photons = init_photons camera
 final_photons   = propagate_photons initial_photons
-output          = data_to_save final_photons camera
+results         = data_to_save final_photons camera
 
 main = do
-    writeFile "data.txt" output
+    writeFile "data.txt" results
