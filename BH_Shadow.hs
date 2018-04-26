793a71dff5afd8a0661953ba5e559049a4ff45b2
Michael <michaeloriordan4@gmail.com>
2018-04-26 18:36:31 +0100

    Small

--- BH_Shadow.hs
+++ BH_Shadow.hs
@@ -157,11 +157,13 @@ step_geodesic_vverlet :: Photon -> Scalar -> Photon
 step_geodesic_vverlet ph dl = phf where
     x = photon_x ph
     k = photon_k ph
+
     dk1 = dkdl x k
 
     xf = zipWith3 (\a b c -> a + dl*b + ((dl^2)/2)*c) x k dk1 
-    k1 = zipWith (\a b -> a + dl*b) k dk1 -- Estimate new k for dkdl
-    dk2 = dkdl xf k1
+
+    dk2 = dkdl xf k1 
+        where k1 = zipWith (\a b -> a + dl*b) k dk1 -- Estimate new k
 
     kf = zipWith3 (\a b c -> a + (dl/2)*(b + c)) k dk1 dk2 
 
