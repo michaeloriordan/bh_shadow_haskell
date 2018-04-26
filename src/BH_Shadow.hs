a7bad4ebd8deedb17e6f9ecaa76e6e50ef8cf7b2
Michael <michaeloriordan4@gmail.com>
2018-04-27 00:34:58 +0100

    Propagate photons

--- src/BH_Shadow.hs
+++ src/BH_Shadow.hs
@@ -6,10 +6,7 @@ import Config
 import Photon
 import Camera
 import Type_Defs
-import Geometry (conn)
-import Geodesic_Equation (dkdl)
-import Geodesic_Integration (step_geodesic, stepsize)
-import Control.Parallel.Strategies (withStrategy,parListChunk,rseq)
+import Propagate_Photons (propagate_photons, photon_escaped, photon_captured)
 
 --------------------------------------------------------------------------------
 
@@ -43,68 +40,6 @@ init_photon k0 cr ci pixel = Photon xi ki where
 
 init_photons :: Pixels -> Photons
 init_photons = map $ init_photon k0_init (distance camera) (inclination camera)
-
---------------------------------------------------------------------------------
-
-step_photon :: Photon -> Photon
-step_photon ph = phf where
-    dl = stepsize (photon_x ph) (photon_k ph)
-    phh = step_geodesic ph dl
-    phf = bound_coords phh
-
---------------------------------------------------------------------------------
-
-bound_coords :: Photon -> Photon
-bound_coords ph = case coords of
-    Schwarzschild_GP -> bound_coords' (photon_x ph) (photon_k ph)
-    Kerr_BL          -> bound_coords' (photon_x ph) (photon_k ph)
-    Kerr_KS          -> bound_coords' (photon_x ph) (photon_k ph)
-
--- Assumes x2 and x3 usual theta and phi
--- Force theta to stay in the domain [0, pi] - Chan et al. (2013)
-bound_coords' :: Vec1 -> Vec1 -> Photon
-bound_coords' x k  
-    | x2 > pi   = Photon [x0, x1, 2*pi-x2, x3+pi] [k0, k1, -k2, k3]
-    | x2 < 0    = Photon [x0, x1,     -x2, x3-pi] [k0, k1, -k2, k3]
-    | otherwise = Photon [x0, x1,      x2,    x3] [k0, k1,  k2, k3]
-    where
-        (x0,x1,x2,x3) = components x
-        (k0,k1,k2,k3) = components k
-
---------------------------------------------------------------------------------
-
-photon_finished :: Photon -> Bool
-photon_finished ph = (photon_escaped ph) || (photon_captured ph)
-
-photon_escaped :: Photon -> Bool
-photon_escaped ph = (photon_r ph) > rmax
-
-photon_captured :: Photon -> Bool
-photon_captured ph = (photon_r ph) <= rmin
-
---------------------------------------------------------------------------------
-
-parmap :: (a -> b) -> [a] -> [b]
-parmap = parmap' chunk_size
-
-parmap' :: Int -> (a -> b) -> [a] -> [b]
-parmap' chunk f = withStrategy (parListChunk chunk rseq) . map f
-
-map' :: (a -> b) -> [a] -> [b]
-map' = if do_parallel then parmap else map
-
---------------------------------------------------------------------------------
-
-propagate_photon' :: Int -> Photon -> Photon
-propagate_photon' n ph 
-    | photon_finished ph || n > nmax = ph
-    | otherwise = propagate_photon' (n+1) $ step_photon ph
-
-propagate_photon :: Photon -> Photon
-propagate_photon = propagate_photon' 0
-
-propagate_photons :: Photons -> Photons
-propagate_photons = map' propagate_photon
 
 --------------------------------------------------------------------------------
 
